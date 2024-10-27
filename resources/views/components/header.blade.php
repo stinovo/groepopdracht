2d8e5d46cc62bcbff5746b6a037f6253317390d9
KevinRudyHsieh <117444524+KevinRudyHsieh@users.noreply.github.com>
2024-10-27 13:27:51 +0100

    ticket 7
    added button

--- resources/views/components/header.blade.php
+++ resources/views/components/header.blade.php
@@ -1,9 +1,12 @@
 <!-- Main jumbotron for a primary marketing message or call to action -->
 <div class="jumbotron">
     <div class="container">
-        <a href="/" title="{{ __('misc.home_alt') }}" alt="{{ __('misc.home_alt') }}">
-            <h1>{{ __('misc.homepage_title') }}</h1>
+        <a href="/" title="{{ __('misc.home_alt') }}" style="text-decoration: none;">
+            <h1 style="font-size: 2.5em; color: #333;">{{ __('misc.homepage_title') }}</h1>
         </a>
-        {{ $introduction_text ?? '' }}
+        <p style="font-size: 1.2em; color: #666;">{{ $introduction_text ?? '' }}</p>
+        <a href="/" title="Download je handleiding: Gratis gebruikersgidsen!" style="display: inline-block; padding: 10px 20px; font-size: 16px; color: #fff; background-color: #4682B4; text-align: center; text-decoration: none; border-radius: 5px;">
+            Download je handleiding
+        </a>
     </div>
 </div>
